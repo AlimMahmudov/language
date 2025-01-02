56c449cb5bcb235770afbbde410bcc8fba3b8c72
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-02 12:59:18 +0600

    two

--- src/appProject/appSections/home/Home.tsx
+++ src/appProject/appSections/home/Home.tsx
@@ -1,13 +1,13 @@
+"use client";
 import { useLanguageStore } from "@/shared/stores/UseLanguageStores";
-import Image from "next/image";
 import React from "react";
 
 const Home = () => {
   const { t } = useLanguageStore();
   return (
     <div>
-      <h1>{t("Hero.title")}</h1>
-      <p>{t("Hero.desc")} </p>
+      <h1>{t("Home.title")}</h1>
+      <p>{t("Home.desc")} </p>
     </div>
   );
 };
--- src/appProject/appSections/select/Select.tsx
+++ src/appProject/appSections/select/Select.tsx
@@ -1,31 +1,23 @@
-// "use client";
-// import { useLanguageStore } from "@/shared/stores/UseLanguageStores";
-// import React from "react";
-
-// const Select = () => {
-//   const { language, setLanguage } = useLanguageStore();
-
-//   const handleChangeLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     const selectedLanguage = e.target.value as "ru" | "en";
-//     setLanguage(selectedLanguage);
-//   };
-
-//   return (
-//     <div>
-//       <select onChange={handleChangeLanguage} value={language}>
-//         <option value="ru">ru</option>
-//         <option value="ky">en</option>
-//       </select>
-//     </div>
-//   );
-// };
-
-// export default Select;
-
+"use client";
+import { useLanguageStore } from "@/shared/stores/UseLanguageStores";
 import React from "react";
 
 const Select = () => {
-  return <div></div>;
+  const { language, setLanguage } = useLanguageStore();
+
+  const handleChangeLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedLanguage = e.target.value as "ru" | "en";
+    setLanguage(selectedLanguage);
+  };
+
+  return (
+    <div>
+      <select onChange={handleChangeLanguage} value={language}>
+        <option value="ru">ru</option>
+        <option value="ky">en</option>
+      </select>
+    </div>
+  );
 };
 
 export default Select;
