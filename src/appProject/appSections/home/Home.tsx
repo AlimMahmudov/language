9c4ba13c1e45f24772f0fcd51693613b5c605423
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-07 22:04:00 +0600

    home container

--- src/appProject/appSections/home/Home.tsx
+++ src/appProject/appSections/home/Home.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useLanguageStore } from "@/shared/stores/UseLanguageStores";
 import React from "react";
+import scss from "./Home.module.scss";
 
 const Home = () => {
   const { t } = useLanguageStore();
   return (
-    <div>
-      <h1>{t("Home.title")}</h1>
-      <p>{t("Home.desc")} </p>
+    <div id={scss.Home}>
+      <div className="container">
+        <div className={scss.home}>
+          <h1>{t("Home.title")}</h1>
+          <p>{t("Home.desc")} </p>
+        </div>
+      </div>
     </div>
   );
 };
