efc8c78567787c72c72cf1d5f8f989b7fe43ae07
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-07 22:17:12 +0600

    home backround

--- src/appProject/appSections/home/Home.tsx
+++ src/appProject/appSections/home/Home.tsx
@@ -13,10 +13,7 @@ const Home = () => {
             src="https://m.media-amazon.com/images/I/91g0NtLvNuL._UF1000,1000_QL80_.jpg"
             alt=""
           />
-          <img
-            src="https://64.media.tumblr.com/7e278e63ff2c9c0b100eaa4bbd0d3948/tumblr_omk5k4Uj9N1tedwcmo9_r1_500.gif"
-            alt=""
-          />
+
           <div className={scss.text}>
             <h1>{t("Home.title")}</h1>
             <p>{t("Home.desc")} </p>
